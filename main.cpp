f248c76957c3f2f440f3c1d87d2debbcf4814f93
WorseCpp <kiranmdonnelly@gmail.com>
2025-06-14 09:53:08 -0700

    done

--- main.cpp
+++ main.cpp
@@ -18,10 +18,15 @@
 
 #include <utility>
 
-constexpr int SCR_WIDTH = 800;
-constexpr int SCR_HEIGHT = 600;
-constexpr float sphereRadius = 10000.0f;
-constexpr float M = .0f;
+constexpr int scr = 5;
+
+int SCR_WIDTH = 800 / scr;
+int SCR_HEIGHT = 600 / scr;
+float sphereRadius = 1000.0f;
+
+float p = 30;
+
+float M = std::pow(10, -p);
 
 struct Star {
     float l;
@@ -119,9 +124,9 @@ class Camera {
             // For antialiasing, generate 4 sample rays within the pixel.
             // Note: i is the row (vertical index) and j is the column.
             std::vector<glm::vec2> offsets = {
-                // glm::vec2(-0.25f, -0.25f),
-                // glm::vec2( 0.25f, -0.25f),
-                // glm::vec2(-0.25f,  0.25f),
+                glm::vec2(-0.25f, -0.25f),
+                glm::vec2( 0.25f, -0.25f),
+                glm::vec2(-0.25f,  0.25f),
                 glm::vec2( 0.25f,  0.25f)
             };
 
@@ -139,7 +144,7 @@ class Camera {
                 glm::vec3 ray_direction = glm::normalize(front + x_ndc * right + y_ndc * up);
 
                 // Create the ray starting at the camera position.
-                rays.emplace_back(position, ray_direction, glm::vec3(.0f), 0.0f);
+                rays.emplace_back(position, ray_direction, glm::vec3(.0f), 1.0f);
             }
 
             return rays;
@@ -160,131 +165,221 @@ bool linear_approx_collides(const Ray& r, const Star& s) {
     return (theta < max_theta && theta > 0.f);
 }
 
-float du_by_dphi_squared(float r, float u)
+float du_by_dphi_squared(float u, float phi)
 {
     return 1.5 * (2 * M) * u * u - u;
 }
 
-// Computes one RK45 step for the second-order ODE:
-//   d²r/dphi² = f(r, phi)
-// by rewriting it as a system:
-//   dr/dphi = u,  du/dphi = f(r, phi)
-// r0   - current value of r
-// dr0  - current value of dr/dphi
-// phi0 - current phi value
-// h    - step size in phi
-// f    - function providing the second derivative given r and phi
-// Returns a tuple: { new_r, new_dr/dphi, new_phi }
-std::tuple<float, float, float> rk45_step(float r0, float dr0, float phi0, float h, 
-    const std::function<float(float, float)>& f) {
+std::tuple<float, float, float> RK4(float u, float phi, float du_by_dphi, float h)
+{
+    // Our state is Y = {u, v} with v = du_by_dphi.
+    // The ODE system is:
+    // u' = v
+    // v' = du_by_dphi_squared(u, phi)
+    auto f = [&](float u_val, float v_val, float phi_val) -> std::pair<float, float>
+    {
+        return { v_val, du_by_dphi_squared(u_val, phi_val) };
+    };
 
-    // k1 for both r and dr/dphi.
-    float k1_r  = h * dr0;
-    float k1_dr = h * f(r0, phi0);
+    // Compute k1 = h * f(u, v, phi)
+    float k1_u, k1_v;
+    {
+        auto [du, dv] = f(u, du_by_dphi, phi);
+        k1_u = h * du;
+        k1_v = h * dv;
+    }
 
-    // k2 computed at phi0 + h/4.
-    float k2_r  = h * (dr0 + 0.25 * k1_dr);
-    float k2_dr = h * f(r0 + 0.25 * k1_r, phi0 + 0.25 * h);
+    // k2 at phi + h/4 with state = {u + k1_u/4, v + k1_v/4}
+    float k2_u, k2_v;
+    {
+        float u2 = u + k1_u * (1.0f/4.0f);
+        float v2 = du_by_dphi + k1_v * (1.0f/4.0f);
+        auto [du, dv] = f(u2, v2, phi + h/4.0f);
+        k2_u = h * du;
+        k2_v = h * dv;
+    }
 
-    // k3 computed at phi0 + 3h/8.
-    float k3_r  = h * (dr0 + (3.0/32.0) * k1_dr + (9.0/32.0) * k2_dr);
-    float k3_dr = h * f(r0 + (3.0/32.0) * k1_r + (9.0/32.0) * k2_r, phi0 + (3.0/8.0) * h);
+    // k3 at phi + 3h/8 with state = {u + 3/32*k1_u + 9/32*k2_u, v + 3/32*k1_v + 9/32*k2_v}
+    float k3_u, k3_v;
+    {
+        float u3 = u + (3.0f/32.0f)*k1_u + (9.0f/32.0f)*k2_u;
+        float v3 = du_by_dphi + (3.0f/32.0f)*k1_v + (9.0f/32.0f)*k2_v;
+        auto [du, dv] = f(u3, v3, phi + 3.0f*h/8.0f);
+        k3_u = h * du;
+        k3_v = h * dv;
+    }
 
-    // k4 computed at phi0 + (12/13)*h.
-    float k4_r  = h * (dr0 + (1932.0/2197.0) * k1_dr - (7200.0/2197.0) * k2_dr + (7296.0/2197.0) * k3_dr);
-    float k4_dr = h * f(r0 + (1932.0/2197.0) * k1_r - (7200.0/2197.0) * k2_r + (7296.0/2197.0) * k3_r, phi0 + (12.0/13.0) * h);
+    // k4 at phi + 12h/13 with state = {u + 1932/2197*k1_u - 7200/2197*k2_u + 7296/2197*k3_u, ...}
+    float k4_u, k4_v;
+    {
+        float u4 = u + (1932.0f/2197.0f)*k1_u - (7200.0f/2197.0f)*k2_u + (7296.0f/2197.0f)*k3_u;
+        float v4 = du_by_dphi + (1932.0f/2197.0f)*k1_v - (7200.0f/2197.0f)*k2_v + (7296.0f/2197.0f)*k3_v;
+        auto [du, dv] = f(u4, v4, phi + 12.0f*h/13.0f);
+        k4_u = h * du;
+        k4_v = h * dv;
+    }
 
-    // k5 computed at phi0 + h.
-    float k5_r  = h * (dr0 + (439.0/216.0)*k1_dr - 8.0 * k2_dr + (3680.0/513.0)*k3_dr - (845.0/4104.0)*k4_dr);
-    float k5_dr = h * f(r0 + (439.0/216.0)*k1_r - 8.0 * k2_r + (3680.0/513.0)*k3_r - (845.0/4104.0)*k4_r, phi0 + h);
+    // k5 at phi + h with state = {u + 439/216*k1_u - 8*k2_u + 3680/513*k3_u - 845/4104*k4_u, ...}
+    float k5_u, k5_v;
+    {
+        float u5 = u + (439.0f/216.0f)*k1_u - 8.0f*k2_u + (3680.0f/513.0f)*k3_u - (845.0f/4104.0f)*k4_u;
+        float v5 = du_by_dphi + (439.0f/216.0f)*k1_v - 8.0f*k2_v + (3680.0f/513.0f)*k3_v - (845.0f/4104.0f)*k4_v;
+        auto [du, dv] = f(u5, v5, phi + h);
+        k5_u = h * du;
+        k5_v = h * dv;
+    }
 
-    // k6 computed at phi0 + h/2.
-    float k6_r  = h * (dr0 - (8.0/27.0)*k1_dr + 2.0 * k2_dr - (3544.0/2565.0)*k3_dr + (1859.0/4104.0)*k4_dr - (11.0/40.0)*k5_dr);
-    float k6_dr = h * f(r0 - (8.0/27.0)*k1_r + 2.0 * k2_r - (3544.0/2565.0)*k3_r + (1859.0/4104.0)*k4_r - (11.0/40.0)*k5_r, phi0 + 0.5 * h);
+    // k6 at phi + h/2 with state = {u - 8/27*k1_u + 2*k2_u - 3544/2565*k3_u + 1859/4104*k4_u - 11/40*k5_u, ...}
+    float k6_u, k6_v;
+    {
+        float u6 = u - (8.0f/27.0f)*k1_u + 2.0f*k2_u - (3544.0f/2565.0f)*k3_u + (1859.0f/4104.0f)*k4_u - (11.0f/40.0f)*k5_u;
+        float v6 = du_by_dphi - (8.0f/27.0f)*k1_v + 2.0f*k2_v - (3544.0f/2565.0f)*k3_v + (1859.0f/4104.0f)*k4_v - (11.0f/40.0f)*k5_v;
+        auto [du, dv] = f(u6, v6, phi + h/2.0f);
+        k6_u = h * du;
+        k6_v = h * dv;
+    }
 
-    // Fourth-order estimate.
-    float r4  = r0  + (25.0/216.0)*k1_r + (1408.0/2565.0)*k3_r + (2197.0/4104.0)*k4_r - (1.0/5.0)*k5_r;
-    float dr4 = dr0 + (25.0/216.0)*k1_dr + (1408.0/2565.0)*k3_dr + (2197.0/4104.0)*k4_dr - (1.0/5.0)*k5_dr;
+    // Combine to produce a 5th order solution:
+    float u_new = u + (16.0f/135.0f)*k1_u + (6656.0f/12825.0f)*k3_u + (28561.0f/56430.0f)*k4_u
+                  - (9.0f/50.0f)*k5_u + (2.0f/55.0f)*k6_u;
 
-    // Fifth-order estimate.
-    float r5  = r0  + (16.0/135.0)*k1_r + (6656.0/12825.0)*k3_r + (28561.0/56430.0)*k4_r - (9.0/50.0)*k5_r + (2.0/55.0)*k6_r;
-    float dr5 = dr0 + (16.0/135.0)*k1_dr + (6656.0/12825.0)*k3_dr + (28561.0/56430.0)*k4_dr - (9.0/50.0)*k5_dr + (2.0/55.0)*k6_dr;
+    float du_by_dphi_new = du_by_dphi + (16.0f/135.0f)*k1_v + (6656.0f/12825.0f)*k3_v + (28561.0f/56430.0f)*k4_v
+                           - (9.0f/50.0f)*k5_v + (2.0f/55.0f)*k6_v;
 
-    float new_phi = phi0 + h;
-    // Return the fifth-order estimates as the new state.
-    return { r5, dr5, new_phi };
+    return std::tuple<float, float, float>(u_new, phi + h, du_by_dphi_new);
 }
 
-glm::vec2 u_phi_to_x_y(float u, float phi)
+std::pair<float, float> u_phi_to_x_y(float u, float phi)
 {
     float r = 1 / u;
     float x = r * std::cos(phi);
     float y = r * std::sin(phi);
-    return glm::vec2(x, y);
+    return std::pair<float, float>(x, y);
+}
+
+std::pair<float, float> x_y_to_u_phi(float x, float y)
+{
+    float r = sqrt(x*x + y*y);
+    float u = 1. / r;
+    float phi = atan2(y, x);
+    return std::pair<float, float>(u, phi);
 }
 
 Ray linearize_ray(const Ray& ray) {
 
     double r0 = glm::length(ray.position);
 
-    double x0 = r0;
+    float x0 = r0;
 
-    double y0 = .0;
+    float y0 = .0;
 
-    double phi0 = .0;
-
-    double u0 = 1. / r0;
-
-    glm::vec3 new_pos = ray.position + ray.direction * 0.01f; // Set the initial position in the xy plane.
-
-    double r1 = glm::length(new_pos);
-
-    double phi1 = glm::acos(glm::dot(new_pos, ray.position) / (r0 * r1));
+    double phi0, u0;
     
-    double u1 = 1.0 / r1;
+    glm::vec3 i_hat = glm::normalize(ray.position);
 
-    double du_dphi = (u1 - u0) / (phi1 - phi0); // Approximate the radial derivative.
+    glm::vec3 plane_normal = glm::normalize(glm::cross(i_hat, ray.direction));
+    
+    glm::vec3 j_hat = glm::cross(plane_normal, i_hat);
 
-    while (true) { // Iterate until the derivative is small enough.
+    assert(std::abs(glm::dot(i_hat, ray.position) - glm::length(ray.position)) < .01);
+
+    assert(std::abs(glm::dot(i_hat, j_hat)) < .01);
+    
+    std::tie(u0, phi0) = x_y_to_u_phi(x0, y0);
+
+    glm::vec3 new_pos = ray.position + ray.direction * 0.0001f; // Set the initial position in the xy plane.
+
+    float x1 = glm::dot(new_pos, i_hat);
+    float y1 = glm::dot(new_pos, j_hat);
+
+    float u1, phi1;
+
+    std::tie(u1, phi1) = x_y_to_u_phi(x1, y1);
+
+    float du_dphi = (u1 - u0) / (phi1 - phi0);
+
+    assert(glm::length(x0 * i_hat + y0 * j_hat - ray.position) < .01);
+    
+    assert(glm::length(x1 * i_hat + y1 * j_hat - new_pos) < .01);
+
+    assert(glm::dot(j_hat, ray.direction) > 0);
+    
+    //std::cout << x0 <<", " << y0 << "," << x1 << " ," << y1 << "\n";
+
+    int ctr = 0;
+
+    while (true) {
         
-        auto [new_u, new_du_dphi, new_phi] = rk45_step(u0, du_dphi, phi0, 0.001, du_by_dphi_squared);
-        
+        float h = .1;
+        redo:
+        int strike = 0;
+        auto [new_u, new_phi, new_du_dphi] = RK4(u0, phi0, du_dphi, h);
+        strike ++;
+
+        if (new_u < 0)
+        {
+            h /= 5;
+
+            if (strike > 5)
+            {
+                return Ray(glm::vec3(sphereRadius * 10), glm::vec3(1.0, 0.0, 0.0), glm::vec3(0.0), 0);
+            }
+
+            goto redo;
+        }
+
+
+        //std::cout << "["<<new_u << ", " << new_phi << "],\n";
+
+        if (std::abs(1 / new_u) > .9 * sphereRadius)
+        {
+
+            float a = ((.9 * sphereRadius) - u0) / (new_u - u0);
+
+            assert(abs((1-a)*u0 + a * new_u - .9*sphereRadius) < .01 * sphereRadius);
+
+            u0 = .9 * sphereRadius;
+
+            du_dphi = (1-a) * du_dphi + a * new_du_dphi;
+
+            phi0 = (1-a) * phi0 + a * new_phi;
+
+            break;
+        }   
+
         u0 = new_u;
         
         du_dphi = new_du_dphi;
         
         phi0 = new_phi;
 
-        if (std::abs(u0) > .9 * sphereRadius)
-            break;
 
         if (u0 != u0){
+            //std::cout << " Nana Out\n";
             return Ray(glm::vec3(sphereRadius * 10), glm::vec3(1.0, 0.0, 0.0), glm::vec3(0.0), 0);
         }
 
-        if (1 / u0 < 2 * M)
+        if (std::abs(1 / u0) < 2 * M)
         {
+            //std::cout << "fell in!\n";
             return Ray(glm::vec3(sphereRadius * 10), glm::vec3(1.0, 0.0, 0.0), glm::vec3(0.0), 0);
         }
     }
 
-    float phi = phi0;
+    float xf, yf;
+    std::tie(xf, yf) = u_phi_to_x_y(u0, phi0);
 
-    float r = 1 / u0;
+    float uf1 = u0 + du_dphi * .0001;
+    float phif1 = phi0 + .0001;
 
-    float x_off = r * glm::cos(phi);
-    
-    float y_off = r * glm::sin(phi);
-    
-    glm::vec3 new_ray_pos = ray.position + x_off * glm::normalize(ray.position) + y_off * glm::normalize(ray.direction);
+    float xf1, yf1;
+    std::tie(xf1, yf1) = u_phi_to_x_y(uf1, phif1);
 
-    glm::vec2 new_ray_0 = u_phi_to_x_y(u0, phi0);
-    
-    glm::vec2 new_ray_1 = u_phi_to_x_y(u0 + du_dphi * 0.01, phi0 + 0.01);
-    
-    glm::vec2 new_dir = new_ray_1 - new_ray_0;
+    //std::cout << xf <<", " << yf << ", " << xf1 << ", " << yf1 << "\n";
 
-    glm::vec3 new_ray_dir = new_dir.x * glm::normalize(ray.position) + new_dir.y * glm::normalize(ray.direction);
+    glm::vec3 new_ray_pos = i_hat * xf1 + j_hat * yf1;
+
+    glm::vec3 new_ray_dir = glm::normalize(i_hat * (xf1 - xf) + j_hat * (yf1 - yf));
     
     return Ray(new_ray_pos, new_ray_dir, ray.color, ray.time);
 }
@@ -297,11 +392,11 @@ glm::vec3 sample(const std::vector<Ray>& rays, const std::vector<Star>& stars) {
 
         auto ray = linearize_ray(rayIm); // Linearize the ray to approximate its path.
 
-        std::cout << "Linearized ray: position(" << ray.position.x << ", " << ray.position.y << ", " << ray.position.z 
-              << "), direction(" << ray.direction.x << ", " << ray.direction.y << ", " << ray.direction.z << ")\n";
-        
-        std::cout << "Original ray: position(" << rayIm.position.x << ", " << rayIm.position.y << ", " << rayIm.position.z 
-              << "), direction(" << rayIm.direction.x << ", " << rayIm.direction.y << ", " << rayIm.direction.z << ")\n";
+        //std::cout << "Linearized ray: position(" << ray.position.x << ", " << ray.position.y << ", " << ray.position.z 
+        //      << "), direction(" << ray.direction.x << ", " << ray.direction.y << ", " << ray.direction.z << ")\n";
+        //
+        //std::cout << "Original ray: position(" << rayIm.position.x << ", " << rayIm.position.y << ", " << rayIm.position.z 
+        //      << "), direction(" << rayIm.direction.x << ", " << rayIm.direction.y << ", " << rayIm.direction.z << ")\n";
 
         // Check each star for a collision (ray-sphere intersection)
         for (const auto& star : stars) {
@@ -310,6 +405,10 @@ glm::vec3 sample(const std::vector<Ray>& rays, const std::vector<Star>& stars) {
                 // If the ray collides with the star, accumulate its color.
                 accumulatedColor += star.c * star.l; // Assuming star.l is the light intensity.
                 break; // Stop checking other stars once we hit one.
+            }
+            else if (glm::length(ray.position) < sphereRadius)
+            {
+                accumulatedColor += glm::vec3(.003f);
             }
         }
     }
@@ -320,29 +419,29 @@ glm::vec3 sample(const std::vector<Ray>& rays, const std::vector<Star>& stars) {
 
 
 
-static std::mt19937 gen(std::random_device{}());
+static std::mt19937 gen(1); //std::random_device{}());
 int main() {
 
 
     Ray testRay(glm::vec3(100.0f, 0.0f, 0.0f), glm::normalize(glm::vec3(-.9f, 0.1f, 0.0f)), glm::vec3(1.0f), 0.0f);
-    std::cout << "Created ray at (" 
-              << testRay.position.x << ", " << testRay.position.y << ", " << testRay.position.z << ")"
-              << " with direction (" 
-              << testRay.direction.x << ", " << testRay.direction.y << ", " << testRay.direction.z << ")" 
-              << std::endl;
+    //std::cout << "Created ray at (" 
+    //          << testRay.position.x << ", " << testRay.position.y << ", " << testRay.position.z << ")"
+    //          << " with direction (" 
+    //          << testRay.direction.x << ", " << testRay.direction.y << ", " << testRay.direction.z << ")" 
+    //          << std::endl;
 
               
     auto linray = linearize_ray(testRay);
 
-    std::cout << "Linearized ray at (" 
-              << linray.position.x << ", " << linray.position.y << ", " << linray.position.z << ")"
-              << " with direction (" 
-              << linray.direction.x << ", " << linray.direction.y << ", " << linray.direction.z << ")"
-              << " and color (" 
-              << linray.color.r << ", " << linray.color.g << ", " << linray.color.b << ")" 
-              << std::endl;
+    //std::cout << "Linearized ray at (" 
+    //          << linray.position.x << ", " << linray.position.y << ", " << linray.position.z << ")"
+    //          << " with direction (" 
+    //          << linray.direction.x << ", " << linray.direction.y << ", " << linray.direction.z << ")"
+    //          << " and color (" 
+    //          << linray.color.r << ", " << linray.color.g << ", " << linray.color.b << ")" 
+    //          << std::endl;
 
-    glm::vec3 pos(-10.0f, 0.0f, 5.0f);
+    glm::vec3 pos(-10.0f, 0.0f, 0.0f);
     glm::vec3 front(1.0f, 0.0f, 0.0f);
     glm::vec3 up(0.0f, 1.0f, 0.0f);
     
@@ -361,29 +460,7 @@ int main() {
     std::uniform_real_distribution<float> offsetDis(-0.1f, 0.1f);
     std::uniform_real_distribution<float> dis(.1f, 1.f);
 
-    //for (int j = 0; j < 10; j++)
-    //{
-    //    // Choose non-zero initial phi to avoid division by zero.
-    //    double r0 = 1.0;
-    //    double phi0 = 0.0;
-    //    double dr0 = 1.0;
-    //    double h = 0.01;
-//
-    //    // Define f from the second derivative:
-    //    // Differentiating dr/dphi = sqrt(phi) gives: d²r/dphi² = 1/(2*sqrt(phi))
-    //    auto f = [](double r, double phi) {
-    //        return phi;
-    //    };
-//
-    //    auto step = rk45_step(r0, dr0, phi0, h, f);
-//
-    //    std::cout << "\nRK45 step result:" << std::endl;
-    //    std::cout << "r = " << std::get<0>(step) << std::endl;
-    //    std::cout << "dr/dphi = " << std::get<1>(step) << std::endl;
-    //    std::cout << "phi = " << std::get<2>(step) << std::endl;
-    //    std::cout << std::exp(std::get<2>(step)) << std::endl;
-    //}
-
+ 
     for (int i = 0; i < numStars; i++) {
         float t = static_cast<float>(i) / static_cast<float>(numStars);
         float inclination = std::acos(1.0f - 2.0f * t);
@@ -417,6 +494,10 @@ int main() {
         stars.push_back(star);
     }
 
+
+    // stars = {Star{1.0f, glm::vec3(1000.0f, 0.0f, 0.0f), glm::vec3(1.0f, 1.0f, 1.0f), 100.0f}};
+
+
     std::cout << "Generated " << stars.size() << " stars." << std::endl;
     for (size_t i = 0; i < stars.size(); ++i) {
         const Star& star = stars[i];
@@ -426,11 +507,10 @@ int main() {
                   << "Radius(" << star.r << ")" << std::endl;
     }
 
-    //stars = {Star{1.0f, glm::vec3(1000.0f, 0.0f, 0.0f), glm::vec3(1.0f, 1.0f, 1.0f), 100.0f}};
-
 
     // Example usage of Camera within main
-    {
+    for (p; p > 0; p--){
+        M = std::pow(10, -p);
 
         // Fill capturedScreen with a simple gradient image.
         for (size_t y = 0; y < camera.capturedScreen.size(); y++) {
@@ -444,7 +524,9 @@ int main() {
         }
 
         // Dump the generated photo to 'output.ppm'
-        dumpPhoto("output.ppm", camera.capturedScreen);
+        std::stringstream ss;
+        ss << "gr-e" << p <<"-" << scr<< ".ppm";
+        dumpPhoto(ss.str(), camera.capturedScreen);
     }
 
     std::cout << "\n";
--- test_geo.py
+++ test_geo.py
@@ -5,24 +5,28 @@ import matplotlib.patches as patches
 
 import matplotlib.pyplot as plt
 
+M = 1e-10
 
-dd = lambda u : 1.5 * u**2 - u
+def u_phi_to_x_y(u, phi):
+    r = 1 / u
+    x = r * np.cos(phi)
+    y = r*np.sin(phi)
+    return x,y
 
-u0 = 1.1
-phi0 = 0.0
+def x_y_to_u_phi(x, y):
+    r = (x**2 + y**2)**.5
+    phi = np.atan2(y,x)
+    return 1/r, phi
 
-r0 = 1 / u0
-x0 = r0 * np.cos(phi0)
-y0 = r0 * np.sin(phi0)
+dd = lambda u : 3 * M * u**2 - u
 
-v = [.99, 0.01]  # Initial velocity in polar coordinates (dr/dphi, d^2r/dphi^2)
-h = .01
-x1 = x0 + h * v[0]
-y1 = y0 + h * v[1]
+x0, y0, x1, y1 = 10, 0,9.9999 ,5.89246e-07
 
-r1 = np.sqrt(x1**2 + y1**2)
-u1 = 1 / r1  # Convert to u = 1/r
-phi1 = np.arctan2(y1, x1)
+xf, yf, xf1, yf1 = 8.03612, 4.28637, 8.03613, 4.28741
+
+u0, phi0 = x_y_to_u_phi(x0,y0)
+
+u1, phi1 = x_y_to_u_phi(x1, y1)
 
 # Velocity in polar coordinates
 du_by_dphi = (u1 - u0) / (phi1 - phi0)
@@ -31,11 +35,58 @@ def odefun(phi, y):
     # y[0] = r, y[1] = dr/dphi
     return [y[1], dd(y[0])]
 
-phi_span = (phi0, phi0 + .01)  # integrate from phi0 to 10
-y0 = [u0, du_by_dphi]
+phi_span = (phi0, phi0 + np.pi*1.5)  # integrate from phi0 to 10
+c_y0 = [u0, du_by_dphi]
 
-sol = solve_ivp(odefun, phi_span, y0, method='RK45', dense_output=True)
 
+hist = np.array(
+[[1.79099, 0.1],
+[3.46409, 0.2],
+[5.10257, 0.3],
+[6.69007, 0.4],
+[8.21073, 0.5],
+[9.64935, 0.6],
+[10.9916, 0.7],
+[12.2239, 0.8],
+[13.3342, 0.9],
+[14.3112, 1],
+[15.1452, 1.1],
+[15.8279, 1.2],
+[16.3525, 1.3],
+[16.7136, 1.4],
+[16.9078, 1.5],
+[16.933, 1.6],
+[16.789, 1.7],
+[16.4773, 1.8],
+[16.001, 1.9],
+[15.3647, 2],
+[14.575, 2.1],
+[13.6396, 2.2],
+[12.568, 2.3],
+[11.3707, 2.4],
+[10.0599, 2.5],
+[8.64853, 2.6],
+[7.15076, 2.7],
+[5.58154, 2.8],
+[3.95654, 2.9],
+[2.29202, 3],
+[0.604598, 3.1],
+[0.265847, 3.12],
+[0.19808, 3.124],
+[0.130309, 3.128],
+[0.0625366, 3.132],
+[0.0489819, 3.1328],
+[0.0354272, 3.1336],
+[0.0218725, 3.1344],
+[0.00831772, 3.1352],
+[0.00560677, 3.13536],
+[0.00289582, 3.13552],
+[0.000184869, 3.13568]]
+)
+
+
+sol = solve_ivp(odefun, phi_span, c_y0, method='BDF', dense_output=True)
+print(sol.t)
 phi_vals = np.linspace(phi_span[0], phi_span[1], 300)
 u_vals = sol.sol(phi_vals)[0]
 r_vals = 1 / u_vals  # Convert to r = 1/u
@@ -48,7 +99,7 @@ plt.xlabel('x')
 plt.ylabel('y')
 plt.title('2D Plot of r,phi Pairs')
 ax = plt.gca()
-max_r = 10
+max_r = 10000 * .1
 # Draw angular grid lines every 30°
 for angle in np.deg2rad(np.arange(0, 360, 30)):
     ax.plot([0, np.cos(angle) * max_r], [0, np.sin(angle) * max_r],
@@ -58,6 +109,20 @@ for r_circle in np.linspace(0, max_r, 5)[1:]:
     circle = patches.Circle((0, 0), r_circle, fill=False,
                             edgecolor='gray', linestyle='--', linewidth=0.5)
     ax.add_patch(circle)
+
+circle = patches.Circle((0, 0), 2*M, fill=True,
+                          edgecolor='black', facecolor='black',
+                          linestyle='-', linewidth=1)
+ax.add_patch(circle)
+
+u_hist = hist[:,0]
+phi_hist = hist[:, 1]
+
+plt.scatter((1/u_hist) * np.cos(phi_hist), (1/u_hist) * np.sin(phi_hist), s = 10)
+
+plt.plot(x0, y0, 'ro', label='Starting Point')
+plt.arrow(x0, y0, 10 * (x1 - x0), (y1 - y0) * 10, head_width=0.5, head_length=1., fc='blue', ec='blue', label='Initial Velocity')
+
 
 plt.plot(x_vals, y_vals)
 plt.axis('equal')
